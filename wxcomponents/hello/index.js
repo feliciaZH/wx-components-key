bba9fd12d23cc3007fcd6b37c8192449c5737977
zhenghui <1334295412@qq.com>
2023-03-07 18:10:22 +0800

    feat: 微信原生小程序循环节点上的key取值索引编译报错

---
+++ wxcomponents/hello/index.js
@@ -0,0 +1,11 @@
+Component({
+  options: {
+      addGlobalClass: true
+  },
+  properties: {
+      list: {
+          type: Array,
+          value: []
+      }
+  }
+});
---
+++ wxcomponents/hello/index.vue
@@ -0,0 +1,27 @@
+<template>
+<uni-shadow-root class="hello-index"><view>
+  <view v-for="(item,index) in (list)" :key="item.index">{{list}}</view>
+</view></uni-shadow-root>
+</template>
+
+<script>
+
+global['__wxVueOptions'] = {components:{}}
+
+global['__wxRoute'] = 'hello/index'
+Component({
+  options: {
+      addGlobalClass: true
+  },
+  properties: {
+      list: {
+          type: Array,
+          value: []
+      }
+  }
+});
+export default global['__wxComponents']['hello/index']
+</script>
+<style platform="mp-weixin">
+
+</style>
